3cce51622c14a7514cf1b273ae882ea4507175b6
msrcosmos <msrcosmos@msrcosmoss-Mac.local>
2018-01-09 16:43:24 +0530

    commit test 2

--- testreopo/testreopo/ViewController.swift
+++ testreopo/testreopo/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // commit test 2
         // Do any additional setup after loading the view, typically from a nib.
     }
 
